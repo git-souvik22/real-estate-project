b5f916ac7f3c09f05a86e026c68cfd6a26e9c71d
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-05 20:01:13 +0530

    nabvar styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import { FaHome, FaReadme, FaSearch, FaSignInAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <h1 className="font-bold text-sm sm:text-xl">
-        <span className="text-slate-500">REAL</span>
-        <span className="text-slate-700">Estate</span>
-      </h1>
-      <form>
-        <input type="search" placeholder="Search..." />
-      </form>
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl">
+            <span className="text-slate-500">REAL</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="search"
+            placeholder="Search..."
+            className="bg-transparent outline-none  w-28 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700">
+              <div className="hover:underline flex flex-col items-center hover:text-slate-400">
+                <FaHome />
+                Home
+              </div>
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700">
+              <div className="hover:underline flex flex-col items-center hover:text-slate-400">
+                <FaReadme />
+                About
+              </div>
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-slate-700">
+              <div className="hover:underline flex flex-col items-center hover:text-slate-400">
+                <FaSignInAlt />
+                Sign In
+              </div>
+            </li>
+          </Link>
+        </ul>
+      </div>
     </header>
   );
 }
