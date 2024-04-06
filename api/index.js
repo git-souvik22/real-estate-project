5a190bcbcd01ef90b11b67df290c061ea1dd6682
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-06 20:45:35 +0530

    .env configuration

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import dotenv from "dotenv";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 mongoose
-  .connect(
-    "mongodb+srv://Souvik2001:cQeGO7STXFZApuv2@souvik.yjvclqj.mongodb.net/estate"
-  )
+  .connect(process.env.DB_URL)
   .then(() => {
     console.log("DB connected successfully");
   })
