9b278d082218d2ede1cc3f98caac17a717ea1bcf
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-06 20:15:37 +0530

    mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
 const app = express();
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://Souvik2001:cQeGO7STXFZApuv2@souvik.yjvclqj.mongodb.net/estate"
+  )
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("API is running on port 3000");
