24692f5361a7712f4a2937743417e98dbe690d1e
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-07 18:47:31 +0530

    user model creation using mongoose schema

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("users", userSchema);
+
+export default User;
