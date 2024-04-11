23800eb015e8200051429a08fb317252d0555b60
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-11 20:41:49 +0530

    duplicate username validation error rendering

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
+// import bcryptjs from "bcryptjs";
 
 export const SignUp = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+    // const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({
       username: username,
       email: email,
-      password: hashedPassword,
+      password: password,
     });
 
     const savedUser = await newUser.save();
@@ -23,6 +23,17 @@ export const SignUp = async (req, res) => {
     if (err.name === "ValidationError") {
       const errors = Object.values(err.errors).map((error) => error.message);
       return res.status(400).json({ errors });
+    }
+    if (err.name === "MongoServerError") {
+      const errorRes = err.errorResponse.errmsg;
+      if (
+        errorRes.includes("E11000 duplicate key error") &&
+        errorRes.includes("username")
+      ) {
+        return res
+          .status(400)
+          .json({ errMessage: "this username is already taken" });
+      }
     }
     res.status(500).send("Server Error");
   }
