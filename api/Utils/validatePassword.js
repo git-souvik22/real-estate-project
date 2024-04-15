292e7e19ef8a2c06e8e3a28986477c7fc50ba99b
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-15 19:29:31 +0530

    password comparison middleware

--- api/Utils/validatePassword.js
+++ api/Utils/validatePassword.js
@@ -2,10 +2,10 @@ import bcryptjs from "bcryptjs";
 
 export const ValidatePass = (password, hashedPassword) => {
   const verifyPassword = bcryptjs.compareSync(password, hashedPassword);
-  if (verifyPassword) {
+  if (verifyPassword === true) {
     return true;
   }
-  if (!verifyPassword) {
+  if (verifyPassword === false) {
     return false;
   }
 };
