27fd479644d254885bc9170316f05659a0464e80
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-16 20:16:34 +0530

    Redux dispatch methods for signin functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { error, loading } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -14,7 +20,7 @@ export default function SignIn() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
+    dispatch(signInStart);
     const res = await fetch("/api/auth/signin", {
       method: "POST",
       headers: {
@@ -25,15 +31,14 @@ export default function SignIn() {
     const data = await res.json();
     const { success } = data;
     if (success === true) {
-      setError(null);
+      dispatch(signInSuccess(data));
       setTimeout(() => {
-        setLoading(false);
-        alert(data.message);
+        alert("Loggedin Successfully");
         navigate("/profile");
       }, 1000);
     }
     if (success === false) {
-      setError(data.message);
+      dispatch(signInFailure(data.message));
     }
   };
 
