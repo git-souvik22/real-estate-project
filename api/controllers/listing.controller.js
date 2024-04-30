6cd76286c61bf75ad100cf092f8fbf46a2a36c39
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-30 19:35:56 +0530

    listing creation API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,16 @@
+import Listing from "../models/listing.model.js";
+
 export const createListing = async (req, res) => {
   try {
+    const listing = await Listing.create(req.body);
+    const saveListingRecord = await listing.save();
+    if (saveListingRecord) {
+      return res.status(201).json({
+        success: true,
+        message: "Listing created successfully",
+        listing: saveListingRecord,
+      });
+    }
   } catch (error) {
     res.status(500).json({
       success: false,
