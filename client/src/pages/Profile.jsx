6ee03ec17189c380a518714b7e85223744960250
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-05-05 20:23:57 +0530

    listings card data loading and styling

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -30,6 +30,7 @@ export default function Profile() {
   const [formData, setFormData] = useState({});
   const [updateStatus, setUpdateStatus] = useState(false);
   const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -132,6 +133,7 @@ export default function Profile() {
         setShowListingsError(true);
         return;
       }
+      setUserListings(data.listings);
     } catch (error) {
       setShowListingsError(true);
     }
@@ -226,6 +228,42 @@ export default function Profile() {
       <p className="text-red-700 mt-5">
         {showListingsError ? "Error showing listings" : ""}
       </p>
+
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-5 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg flex justify-between gap-4 items-center p-3"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col">
+                <button className="text-red-700 uppercase text-sm">
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase text-sm">
+                  Edit
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
