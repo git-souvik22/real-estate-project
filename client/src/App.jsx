e8c9279b0d34343727d6b8688d6f03055ed8d9e3
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-05-10 20:24:09 +0530

    update listing client route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Private from "./components/Private";
 import CreateListing from "./pages/CreateListing";
+import { UpdateListing } from "./pages/UpdateListing";
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route element={<Private />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing" element={<UpdateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const UpdateListing = () => {
   return <div>UpdateListing</div>;
 };
