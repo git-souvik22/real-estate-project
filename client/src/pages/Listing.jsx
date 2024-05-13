21b462f0c7f69a3bb10b9267dc00f559426a3882
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-05-13 19:37:40 +0530

    listing page UI UX

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Listing() {
-  return <div>Listing</div>;
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setTimeout(() => {
+          setListing(data.listing);
+          setLoading(false);
+        }, 1500);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, []);
+  return (
+    <main>
+      {loading && (
+        <p className="text-center my-4 font-semibold text-2xl">Loading...</p>
+      )}
+      {error && (
+        <p className="text-center my-4 font-semibold text-red-700 text-2xl">
+          Something went wrong!
+        </p>
+      )}
+      {listing && listing.name}
+    </main>
+  );
 }
