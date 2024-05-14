025a0b50ed596277d99fcdd4bb610b8002fed732
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-05-14 19:39:54 +0530

    react swiper image slider

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/swiper-bundle.css";
 
 export default function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const params = useParams();
+  SwiperCore.use([Navigation]);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -37,7 +42,22 @@ export default function Listing() {
           Something went wrong!
         </p>
       )}
-      {listing && listing.name}
+      {listing && !loading && !error && (
+        <>
+          <Swiper navigation={true}>
+            {listing.imageUrls.map((image, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${image}) center no-repeat`,
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
     </main>
   );
 }
